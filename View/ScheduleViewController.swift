4bb72bbda784e4c3e86a9077783204e4e01ebd11
Kashan Qamar <kashanqamar>
2020-12-03 13:00:08 +0000

    handling greeting message.

--- View/ScheduleViewController.swift
+++ View/ScheduleViewController.swift
@@ -55,7 +55,7 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     
     // MARK:- Setup UI
     @objc func setupUI() {
-        self.userNameLabel.text = UserDefaults.standard.getUserSurname() + ", " + UserDefaults.standard.getUserForname()
+        self.userNameLabel.text = getGreeting() + ", " + UserDefaults.standard.getUserSurname() + " " + UserDefaults.standard.getUserForname()
         let date = Date(timeIntervalSince1970: (self.adherenceData.first?.alarm_time ?? 0.0 / 1000.0))
         self.scheduledDateLabel.text = date.toString(dateFormat: "EEEE, MMMM dd")
     }
@@ -123,6 +123,34 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
+    
+    
+    
+    func getGreeting() -> String {
+            let date = NSDate()
+            let calendar = NSCalendar.current
+            let currentHour = calendar.component(.hour, from: date as Date)
+            let hourInt = Int(currentHour.description)!
+            var greeting = ""
+
+            if hourInt >= 12 && hourInt <= 16 {
+                greeting = "Good Afternoon"
+            }
+            else if hourInt >= 7 && hourInt <= 12 {
+                greeting = "Good Morning"
+            }
+            else if hourInt >= 16 && hourInt <= 20 {
+                greeting = "Good Evening"
+            }
+            else if hourInt >= 20 && hourInt <= 24 {
+                greeting = "Good Night"
+            }
+            else if hourInt >= 0 && hourInt <= 7 {
+                greeting = "You should be sleeping right now"
+            }
+
+            return greeting
+        }
     
 
 }
